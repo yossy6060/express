29149c4d1c0d3eb36332ff004a9ad015109012a5
yossy6060 <2301220005vl@cyber-u.ac.jp>
2024-12-03 04:26:14 +0900

    add someting

--- routes/dog.js
+++ routes/dog.js
@@ -1,9 +1,13 @@
+var express = require('express');
+var router = express.Router();
 const request = require('request');
-request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
+router.get('/', async (req, res) => {
+request('https://dog.ceo/api/breeds/image/random', function (error, response,
+body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
-console.log(body);
-const catImageUrl = data[0].url;
-console.log(catImageUrl);
+res.json(data);
 }
 });
+})
+module.exports = router;
